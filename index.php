1f1ef8606ca95f82346defe2d327f3abf8f27f1b
elxsdk <elshindoenk@gmail.com>
2025-02-02 09:39:58 +0700

    menambahkan list pada index.php

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
   <h1>hia ganteng!</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptas dolor architecto modi placeat, iste voluptatibus repudiandae consequuntur facere error nam possimus, eum, corrupti nobis molestias blanditiis quidem eius minima!</p>
+  <ul>
+    <li>minji</li>
+    <li>haerin</li>
+  </ul>
 </body>
 
 </html>
